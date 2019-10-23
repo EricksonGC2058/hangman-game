647917455af0785d2bc845fe9863ca7febe9b0f0
Garrett Erickson <56050410+EricksonGC2058@users.noreply.github.com>
2019-10-23 14:55:07 -0700

    Add files via upload
    new features added

--- hangman.py
+++ hangman.py
@@ -2,8 +2,9 @@ import random
 import os
 guessWord = []
 myWord = ["peter", "lois", "chris", "meg", "stewie", "brian", "vinny", "joe", "quagmire", "cleveland", "mort", "herbert"]
-myList = list(myWord)
-random.choice(myWord)
+theWord = random.choice(myWord)
+misses = 0
+theWord = list(theWord)
 
 frameList = [
 '''
@@ -33,40 +34,50 @@ frameList = [
       ===''','''
    +---+
    O   |
-  /|\\   |
+  /|\\  |
        | 
        |
       ===''','''
    +---+
    O   |
-  /|\\   |
+  /|\\  |
   /    |   
        |
       ===''','''
    +---+
    O   |
-  /|\\   |
-  / \\   |   
+  /|\\  |
+  / \\  |   
        |
       ==='''
 ]
 
-for a in myWord:
+while misses < 7:
+  print(frameList[misses])
+  guess = input("Guess a letter: ")
+  if guess in theWord:
+    theWord.append(guess)
+    print("That letter is in the word")
+  else:
+    misses = misses + 1
+    print("That letter is not in the word")
+
+for a in theWord:
   guessWord.append("_")
 
 choice = input("Type a word: ")
-if choice == myWord:
+if choice == theWord:
   print("It's a match!")
 else:
   print("Not a match")
 letter = input("Type a letter: ")
-if letter in myWord:
+if letter in theWord:
   print("Letter is in the word, nice job!")
 else:
   print("Letter is not in the word")
 
 count = 0
-for s in myWord:
+for s in theWord:
   if s == letter:
     print(count)
   count += 1
