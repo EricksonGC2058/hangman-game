0185c1c54e1aa422774df2fb336e1224b63e0b82
Garrett Erickson <56050410+EricksonGC2058@users.noreply.github.com>
2019-10-24 14:57:58 -0700

    Add files via upload
    updated 10/24

--- hangman.py
+++ hangman.py
@@ -1,10 +1,11 @@
-import random
 import os
-guessWord = []
-myWord = ["peter", "lois", "chris", "meg", "stewie", "brian", "vinny", "joe", "quagmire", "cleveland", "mort", "herbert"]
-theWord = random.choice(myWord)
 misses = 0
-theWord = list(theWord)
+theWord = list("quagmire")
+guessList = list("_______")
+
+print(guessList)
+
+frame = []
 
 frameList = [
 '''
@@ -56,30 +57,20 @@ while misses < 7:
   print(frameList[misses])
   guess = input("Guess a letter: ")
   if guess in theWord:
-    theWord.append(guess)
-    print("That letter is in the word")
+    index = 0
+    for letter in theWord:
+      if letter == guess:
+        guessList[index] = guess
+        print(guessList)
+      index += 1
   else:
     misses = misses + 1
-    print("That letter is not in the word")
-
-for a in theWord:
-  guessWord.append("_")
-
-choice = input("Type a word: ")
-if choice == theWord:
-  print("It's a match!")
-else:
-  print("Not a match")
-letter = input("Type a letter: ")
-if letter in theWord:
-  print("Letter is in the word, nice job!")
-else:
-  print("Letter is not in the word")
 
 count = 0
-for s in theWord:
-  if s == letter:
-    print(count)
-  count += 1
 
-print(guessWord)
+
+if frame == frameList[6]:
+  print("Game over")
+  print(theWord)
+
+print(guessList)
